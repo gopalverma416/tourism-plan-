6e8e10845268bd44b0c83d4ddf6230700abeaad3
Gopal verma <gopalverma@Gopals-MacBook-Air.local>
2024-06-19 15:11:30 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,49 @@
+
+const data=[
+{
+    id: 1,
+    name:"agra",
+    info:"Agra (/ˈɑːɡrə/, Hindustani: [ˈaːgraː] ⓘ) is a city on the banks of the Yamuna river in the Indian state of Uttar Pradesh, about 230 kilometres (140 mi) south-east of the national capital Delhi and 330 km west of the state capital Lucknow. With a population of roughly 1.6 million, Agra is the fourth-most populous city in Uttar Pradesh and twenty-third most populous city in India.[10]",
+    image:"https://lh3.googleusercontent.com/p/AF1QipN4sO59P-VTi9bc9Qgx8rrQ_1J4EnM44Bts5avz=s1360-w1360-h1020",
+    price:"37000",
+
+},
+
+{
+    id: 2,
+    name:"jaipur",
+    info:"jaipur is city jhbk ijhoihjgb hgk hguihui gub guih ugi higp9g98 hvhvu aipur is the capital of India’s Rajasthan state. It evokes the royal family that once ruled the region and that, in 1727, founded what is now called the Old City, or “Pink City” for its trademark building color. At the center of its stately street grid (notable in India) stands the opulent, colonnaded City Palace complex. With gardens, courtyards and museums, part of it is still a royal residence.",
+    image:"https://upload.wikimedia.org/wikipedia/commons/4/41/East_facade_Hawa_Mahal_Jaipur_from_ground_level_%28July_2022%29_-_img_01.jpg",
+    price:"40000",
+
+},
+{
+    id: 3,
+    name:"goa",
+    info:"Goa (Konkani pronunciation: [ɡõːj], Portuguese: [ˈɡoɐ] ⓘ) is a state on the southwestern coast of India within the Konkan region, geographically separated from the Deccan highlands by the Western Ghats.[7][8] It is bound by the Indian states of Maharashtra to the north, and Karnataka to the east and south, with the Arabian Sea in the west. It is India's smallest state by area and fourth-smallest by population. Goa has the highest GDP per capita among all Indian states,[4][9] two and a half times as high as the GDP per capita of the country as a whole.[10] The Eleventh Finance Commission of India named Goa the best-placed state because of its infrastructure, and India's National Commission on Population rated it as having the best quality of life in India (based on the commission'",
+    image:"https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/BeachFun.jpg/576px-BeachFun.jpg",
+    price:"75000",
+
+
+},
+{
+    id: 4,
+    name:"kerala",
+    info:"Kerala, a state on India's tropical Malabar Coast, has nearly 600km of Arabian Sea shoreline. It's known for its palm-lined beaches and backwaters, a network of canals. Inland are the Western Ghats, mountains whose slopes support tea, coffee and spice plantations as well as wildlife. National parks like Eravikulam and Periyar, plus Wayanad and other sanctuaries, are home to elephants, langur monkeys and tigers.",
+    image:"https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcRJB4DyvnjbcjHhJJJdJ29OGKuW4qOj6mjj9HvwRDlcfGrBkoU1kX65SQG6D10zE_nn3LWhk7qlcNYG4gQF5VGAF5zYgz_WmFqpCOhmOz0",
+    price:"65000",
+    
+
+},
+{
+    id: 5,
+    name:"oddisaa",
+    info:"The ancient kingdom of Kalinga, which was invaded by the Mauryan Emperor Ashoka in 261 BCE resulting in the Kalinga War, coincides with the borders of modern-day Odisha.[22] The modern boundaries of Odisha were demarcated by the Indian government when Orissa Province was established on 1 April 1936, consisting of the Odia-speaking districts of Bihar and Orissa Province.[22] The 1st of April is celebrated as Utkala Dibasa (lit. 'Odisha Day').[23] Cuttack was made the capital of the region by Anantavarman Chodaganga in c. 1135,[24] after which the city was used as the capital by many rulers, through the British era until 1948. Thereafter, Bhubaneswar became the capital of Odisha.[25] The ",
+    image:"https://images.unsplash.com/photo-1617217139357-b77ae58ad4b2?q=80&w=2832&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    price:"90000",
+    
+
+},
+
+];
+export default data;
